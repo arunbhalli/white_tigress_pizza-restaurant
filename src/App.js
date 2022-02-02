1ca2ab9ee1be334d60e4bf2d17951302ab469aa8
arunbhalli <tomarbhalli@gmail.com>
2022-02-02 12:30:15 +0100

    adds material ui for icons and header adds header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-
 import React from 'react';
-
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 const App = () => {
-  return <div className='App'>Hello world</div>;
+  return (
+    <div className="App">
+      hello world
+      <div>
+        <AccountCircleIcon />
+      </div>
+    </div>
+  );
 };
 
 export default App;
